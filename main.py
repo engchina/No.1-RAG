504b3fc55d12aad9f3f27fcf6480dc50afd36e5d
engchina <engchina>
2024-07-01 18:42:01 +0800

    update

---
+++ main.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch(server_name="0.0.0.0", server_port=8080)
---
+++ main.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup /home/oracle/miniconda/envs/no.1-rag/bin/python /home/oracle/No.1-RAG/main.py --host 0.0.0.0 --port 8000 &
