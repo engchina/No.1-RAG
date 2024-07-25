32f7467622038ea3a5ce8c091e9c2610185ef137
engchina <engchina>
2024-07-25 13:03:41 +0800

    update

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup /u01/aipoc/miniconda/envs/no.1-rag/bin/python /u01/aipoc/No.1-RAG/main.py --host 0.0.0.0 --port 8080 &
+nohup /u01/aipoc/miniconda/envs/no.1-rag/bin/python /u01/aipoc/No.1-RAG/main_jp.py --host 0.0.0.0 &
 exit 0
