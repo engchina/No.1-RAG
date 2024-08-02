61e5b4266f0696445c051e849f07a64e2646e066
engchina <atjapan2015@gmail.com>
2024-08-02 13:52:12 +0800

    update

---
+++ terraform/stack/datasources.tf
@@ -0,0 +1,22 @@
+data "template_file" "cloud_init_file" {
+  template = file("./cloud_init/bootstrap.template.yaml")
+
+  vars = {
+#     oci_database_autonomous_database_connection_string = base64gzip("admin/${random_string.autonomous_data_warehouse_admin_password.result}@${lower(var.adb_name)}_high")
+    oci_database_autonomous_database_connection_string = base64gzip("admin/${var.adb_password}@${lower(var.adb_name)}_high")
+    oci_database_autonomous_database_wallet_content    = oci_database_autonomous_database_wallet.generated_autonomous_data_warehouse_wallet.content
+    output_compartment_ocid = var.compartment_ocid
+  }
+}
+
+
+data "template_cloudinit_config" "cloud_init" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "bootstrap.yaml"
+    content_type = "text/cloud-config"
+    content      = data.template_file.cloud_init_file.rendered
+  }
+}
---
+++ terraform/stack/variables.tf
@@ -0,0 +1,51 @@
+variable "availability_domain" {
+  default = "bxtG:AP-TOKYO-1-AD-1"
+}
+
+variable "compartment_ocid" {
+  default = ""
+}
+
+variable "adb_name" {
+  default = "AIPOCADB"
+}
+
+variable "adb_password" {
+  default = ""
+}
+
+variable "instance_display_name" {
+  default = "AIPOC_INSTANCE"
+}
+
+variable "instance_shape" {
+  default = "VM.Standard.E4.Flex"
+}
+
+variable "instance_flex_shape_ocpus" {
+  default = 2
+}
+
+variable "instance_flex_shape_memory" {
+  default = 16
+}
+
+variable "instance_boot_volume_size" {
+  default = 100
+}
+
+variable "instance_boot_volume_vpus" {
+  default = 20
+}
+
+variable "instance_image_source_id" {
+  default = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaaoiusqhftxmiyjlulnxx5mdnqfv6pjx4hdcoks3exn7gsrcwpkpdq"
+}
+
+variable "subnet_ai_subnet_id" {
+  default = ""
+}
+
+variable "ssh_authorized_keys" {
+  default = ""
+}
