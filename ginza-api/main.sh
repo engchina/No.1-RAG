03e69fbb90af86a502df6f2a76b8b12f062dce48
engchina <atjapan2015@gmail.com>
2024-08-19 08:54:34 +0800

    update

--- ginza-api/main.sh
+++ ginza-api/main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /u01/aipoc/No.1-RAG/ginza-api && nohup /u01/aipoc/miniconda/envs/ginza-api/bin/python -m uvicorn main:app --host localhost --port 7932 &
+cd /u01/aipoc/No.1-RAG/ginza-api && nohup /u01/aipoc/miniconda/envs/ginza-api/bin/python -m uvicorn main:app --host localhost --port 7932 > /u01/aipoc/No.1-RAG/ginza.log 2>&1 &
+ &
 exit 0
--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup /u01/aipoc/miniconda/envs/no.1-rag/bin/python /u01/aipoc/No.1-RAG/main.py --host 0.0.0.0 &
+nohup /u01/aipoc/miniconda/envs/no.1-rag/bin/python /u01/aipoc/No.1-RAG/main.py --host 0.0.0.0 > /u01/aipoc/No.1-RAG/main.log 2>&1 &
 exit 0
