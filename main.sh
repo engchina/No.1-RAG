a69525f0e44a494ce75ca0fc5e8f52c754e70ade
root <root@thinkpad-pc.localdomain>
2026-04-07 10:01:46 +0900

    update

--- main.sh
+++ main.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-nohup /u01/aipoc/miniconda/envs/no.1-rag/bin/python /u01/aipoc/No.1-RAG/main.py --host 0.0.0.0 > /u01/aipoc/No.1-RAG/main.log 2>&1 &
+
+PROJECT_DIR="/u01/aipoc/No.1-RAG"
+
+cd "$PROJECT_DIR" || exit 1
+nohup /u01/aipoc/miniconda/envs/no.1-rag/bin/python main.py --host 0.0.0.0 > /u01/aipoc/No.1-RAG/main.log 2>&1 &
 exit 0
